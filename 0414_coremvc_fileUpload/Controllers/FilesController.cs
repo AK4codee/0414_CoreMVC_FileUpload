c1c0a7baa892a5940c836caece07ed5e339ce7af
郭家秀 <a880318@gmail.com>
2022-04-14 10:44:37 +0800

    basic controller and html

---
+++ 0414_coremvc_fileUpload/Controllers/FilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace _0414_coremvc_fileUpload.Controllers
+{
+    public class FilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            return View();
+        }
+    }
+}
