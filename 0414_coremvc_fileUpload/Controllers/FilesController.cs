aae2a1f777d06d111d96df8d4091febacd9d9e73
郭家秀 <a880318@gmail.com>
2022-04-14 11:56:43 +0800

    file upload status controller

--- 0414_coremvc_fileUpload/Controllers/FilesController.cs
+++ 0414_coremvc_fileUpload/Controllers/FilesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace _0414_coremvc_fileUpload.Controllers
@@ -14,7 +16,29 @@ namespace _0414_coremvc_fileUpload.Controllers
         [HttpPost]
         public async Task<IActionResult> UploadFile(IFormFile file)
         {
-            return View();
+            if(file == null || file.Length == 0)
+            {
+                ViewData["Message"] = "Upload file' length is zero, Failed!";
+                return View("UploadResult");
+            }
+
+            string pathFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploadFiles", file.FileName);
+
+            try
+            {
+                using(var stream = new FileStream(pathFile, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                ViewData["Message"] = "upload file sccess!";
+            }
+            catch (Exception ex)
+            {
+                ViewData["Message"] = "upload file faled!" + ex.Message;
+            }
+
+            return View("UploadResult");
         }
     }
 }
